0d7fef46aff08a8e527e3ef50603be42a4601c69
Jessica Isoton <jessicasampaio195@gmail.com>
2020-04-22 14:43:58 +0200

    2.9*: The Phonebook Step4- filter updated

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-123456", id: 1 },
-    { name: "Ada Lovelace", phone: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", phone: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", phone: "39-23-6423122", id: 4 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
-  const [newPhone, setNewPhone] = useState("");
+  const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
 
-    if (!persons.find((x) => x.name.localeCompare(newName) === 0)) {
+    if (!persons.find((x) => x.name.toUpperCase() === newName.toUpperCase())) {
       const nameObject = {
         name: newName,
-        phone: newPhone != undefined ? newPhone : "",
+        number: newNumber !== undefined ? newNumber : "",
         id: persons.length + 1,
       };
       setPersons(persons.concat(nameObject));
@@ -25,15 +25,15 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`);
     }
     setNewName("");
-    setNewPhone("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
-  const handlePhoneChange = (event) => {
-    setNewPhone(event.target.value);
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const handleFilterNameChange = (event) => {
@@ -44,7 +44,7 @@ const App = () => {
     filterName == ""
       ? persons
       : persons.filter(
-          (person) => person.name.toUpperCase() === filterName.toUpperCase()
+          (person) => person.name.toUpperCase().startsWith(filterName.toUpperCase())
         );
 
   return (
@@ -59,7 +59,7 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          phone: <input value={newPhone} onChange={handlePhoneChange} />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -68,7 +68,7 @@ const App = () => {
       <h2>Numbers</h2>
       {namesToShow.map((person) => (
         <div key={person.id}>
-          {person.name} {person.phone}
+          {person.name} {person.number}
         </div>
       ))}
     </div>
