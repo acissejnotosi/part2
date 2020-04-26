f305c32676f65bdbc43714af538cf7d531384489
Jessica Isoton <jessicasampaio195@gmail.com>
2020-04-26 22:27:09 +0200

    2.12* Data for countries, step1

--- dataforcountries/src/App.js
+++ dataforcountries/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const shortid = require("shortid");
+
+const CountryInfo = ({ country }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      {country.map((x) => (
+        <div key={shortid.generate()}>
+          <h2>{x.name}</h2>
+          <div>capital {x.capital}</div>
+          <div>population {x.population}</div>
+          <h3>languages</h3>
+          <ul>
+            {x.languages.map((x) => (
+              <li key={shortid.generate()}> {x.name} </li>
+            ))}
+          </ul>
+          <img src={x.flag} height="100" width="100" />
+        </div>
+      ))}
+    </>
+  );
+};
+
+const Filter = (props) => {
+  const showFilter =
+    props.filterCountry === ""
+      ? []
+      : props.countries.filter((x) =>
+          x.name.toUpperCase().includes(props.filterCountry.toUpperCase())
+        );
+  if (showFilter !== []) {
+    if (showFilter.length > 1 && showFilter.length <= 10) {
+      return showFilter.map((x) => (
+        <div key={shortid.generate()}>{x.name}</div>
+      ));
+    } else if (showFilter.length === 1) {
+      return <CountryInfo country={showFilter} />;
+    } else if (showFilter.length > 10) {
+      return <div> Too many maches, specify another filter</div>;
+    }
+  }
+  return null;
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filterCountry, setFilterCountry] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;capital;population;languages;flag"
+      )
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
+
+  const handleFilterCountry = (event) => {
+    setFilterCountry(event.target.value);
+  };
+
+  return (
+    <div>
+      find countries
+      <input value={filterCountry} onChange={handleFilterCountry}></input>
+      <Filter countries={countries} filterCountry={filterCountry} />
     </div>
   );
-}
+};
 
 export default App;
